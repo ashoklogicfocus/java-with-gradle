fe9601f5ad2a214392985bc8153647696dd4c075
ashoklogicfocus <121782440+ashoklogicfocus@users.noreply.github.com>
2023-03-29 10:19:27 +0530

    Update helloWorld.groovy
    Signed-off-by: ashoklogicfocus <121782440+ashoklogicfocus@users.noreply.github.com>

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,34 @@
-def call(Map config = [:]) {
 
-    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+library identifier: 'shared-libs@master',
+    // 'mylibraryname' is just an identifier, it can be anything you like
+    // 'master' refers to a valid git ref (branch)
+    retriever: modernSCM([
+      $class: 'GitSCMSource',
+      credentialsId: '', // remove this if it's public!
+      remote: 'https://github.com/ashoklogicfocus/java-with-gradle.git'
+])
 
+pipeline {
+    agent any
+    tools {
+        gradle 'Gradle 7.4.2'
+    }
+
+    stages {
+        stage('checkout') {
+            steps {
+                checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/ashoklogicfocus/java-with-gradle.git']])
+            }
+        }
+        stage('compile') {
+          steps {
+            step([$class: 'GitHubCommitStatusSetter', statusResultSource: [$class: 'ConditionalStatusResultSource', results: []]])
+        }
+    }
+    stage('test') {
+        steps {
+            echo "test"
+        }
+    }
+}
 }
